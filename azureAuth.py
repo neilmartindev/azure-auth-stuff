e4dd997d089558dbf7744dc983b500bea7d8f880
Neil Martin <87085416+neilmartindev@users.noreply.github.com>
2023-01-31 14:28:13 +0000

    Update azureAuth.py

--- azureAuth.py
+++ azureAuth.py
@@ -1,28 +1,56 @@
-import os
-import msal
 import requests
+import json
 
-AUTHORITY = "https://login.microsoftonline.com/{}".format(os.environ['TENANT_ID'])
-CLIENT_ID = os.environ['CLIENT_ID']
-CLIENT_SECRET = os.environ['CLIENT_SECRET']
-RESOURCE = os.environ['RESOURCE_ID']
+# Step 1: Obtain an authorization code
+# Replace {client_id} with the application's client ID
+# Replace {redirect_uri} with the application's registered redirect URI
+authorization_url = f"https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope=openid%20offline_access%20https://graph.microsoft.com/.default"
 
-app = msal.ConfidentialClientApplication(CLIENT_ID, authority=AUTHORITY, client_credential=CLIENT_SECRET)
+# Step 2: Exchange the authorization code for an access token
+# Replace {client_id} with the application's client ID
+# Replace {client_secret} with the application's client secret
+# Replace {redirect_uri} with the application's registered redirect URI
+# Replace {authorization_code} with the authorization code obtained in step 1
+token_url = "https://login.microsoftonline.com/common/oauth2/v2.0/token"
+data = {
+    "client_id": client_id,
+    "client_secret": client_secret,
+    "redirect_uri": redirect_uri,
+    "code": authorization_code,
+    "grant_type": "authorization_code"
+}
+response = requests.post(token_url, data=data)
 
-result = app.acquire_token_for_client(scopes=[RESOURCE])
+# Step 3: Use the access token and refresh token to authenticate the user in subsequent requests to Microsoft Graph API
+access_token = response.json()["access_token"]
+refresh_token = response.json()["refresh_token"]
+headers = {
+    "Authorization": f"Bearer {access_token}",
+    "Content-Type": "application/json"
+}
 
-access_token = result.get("access_token")
-refresh_token = result.get("refresh_token")
+# Helper function to refresh the access token using the refresh token
+def refresh_access_token():
+    data = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "redirect_uri": redirect_uri,
+        "refresh_token": refresh_token,
+        "grant_type": "refresh_token"
+    }
+    response = requests.post(token_url, data=data)
+    access_token = response.json()["access_token"]
+    return access_token
 
-headers = {"Authorization": "Bearer " + access_token}
-response = requests.get("https://your_resource_endpoint", headers=headers)
+# Step 4: Make a request to Microsoft Graph API using the access token
+graph_url = "https://graph.microsoft.com/v1.0/me"
+try:
+    response = requests.get(graph_url, headers=headers)
+except requests.exceptions.RequestException as e:
+    # If the request fails, refresh the access token and retry the request
+    access_token = refresh_access_token()
+    headers["Authorization"] = f"Bearer {access_token}"
+    response = requests.get(graph_url, headers=headers)
 
-print(response.text)
-
-# Check if the access token has expired
-if response.status_code == 401:
-    result = app.acquire_token_by_refresh_token(refresh_token, scopes=[RESOURCE])
-    access_token = result.get("access_token")
-    headers = {"Authorization": "Bearer " + access_token}
-    response = requests.get("https://your_resource_endpoint", headers=headers)
-    print(response.text)
+# Step 5: Process the response
+print(json.dumps(response.json(), indent=2))
